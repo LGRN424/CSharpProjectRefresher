bcf6d1dc84ce577810992481f26c72f273730467
LGRN424 <LGRN424@GMAIL.COM>
2022-04-03 12:16:08 +0300

    Update account.cs

--- account.cs
+++ account.cs
@@ -4,7 +4,7 @@ class Program
 {
 	static void Main()
 	{
-		Console.WriteLine("Welcome to Acme Accouting Systems" n/Remeber, we're \"accounting\" on you");
+		Console.WriteLine("Welcome to Acme Accouting Systems n/Remeber, we're \"accounting\" on you");
 	}
 
 }
